9e49d4ced1c92e52fc7fcb21065d2e996b5fdb57
Robin JOSEPH <rjrdutg@gmail.com>
2024-05-19 15:47:04 +0200

    🔧 Add 'is_admin' field to user table in init.sql
    The database configuration file init.sql has been modified to include a new field 'is_admin' in the user table structure. This is a boolean field that will hold administrative status for each user in the database.

--- init.sql
+++ init.sql
@@ -7,7 +7,8 @@ CREATE TABLE IF NOT EXISTS users (
                                      username VARCHAR(255) NOT NULL,
                                      password VARCHAR(255) NOT NULL,
                                      email VARCHAR(255) NOT NULL,
-                                     address VARCHAR(255) NOT NULL
+                                     address VARCHAR(255) NOT NULL,
+                                     is_admin BOOLEAN NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS products (
