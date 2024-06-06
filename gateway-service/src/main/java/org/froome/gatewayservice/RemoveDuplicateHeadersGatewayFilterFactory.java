1b75e88cc09d85aca03666f48566687fb3154e54
Robin JOSEPH <rjrdutg@gmail.com>
2024-06-06 14:43:33 +0200

    🔄 Replaced ModifyResponseBody with RemoveDuplicateHeaders in gateway-service
    In the configuration of the gateway-service, the filter ModifyResponseBody has been removed and, in its place, RemoveDuplicateHeaders has been added. Also, the global CORS policy was updated to include a maxAge property.

---
+++ gateway-service/src/main/java/org/froome/gatewayservice/RemoveDuplicateHeadersGatewayFilterFactory.java
@@ -0,0 +1,40 @@
+package org.froome.gatewayservice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.stream.Collectors;
+
+@Component
+@Slf4j
+public class RemoveDuplicateHeadersGatewayFilterFactory extends AbstractGatewayFilterFactory<RemoveDuplicateHeadersGatewayFilterFactory.Config> {
+
+    public RemoveDuplicateHeadersGatewayFilterFactory() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            ServerHttpResponse response = exchange.getResponse();
+            log.info("Headers before removal: {}", response.getHeaders());
+            response.getHeaders().forEach((key, value) -> {
+                if ("Vary".equalsIgnoreCase(key)) {
+                    response.getHeaders().remove(key);
+                } else {
+                    response.getHeaders().put(key, value);
+                }
+            });
+            log.info("Headers after removal: {}", response.getHeaders());
+            return chain.filter(exchange);
+        };
+    }
+
+
+    public static class Config {
+    }
+}
